621a8c08550442884377c7e84a6e373117b13564
Icakis <Icakis@users.noreply.github.com>
2015-01-06 22:30:33 +0200

    Add the all Problems solutions

---
+++ AngularJS Lab/app/js/controllers/RegisterController.js
@@ -0,0 +1,25 @@
+﻿'use strict';
+
+app.controller('RegisterController',
+    function ($scope, $location, townsService, authService, notifyService) {
+        $scope.userData = { townId: null };
+
+        townsService.getTowns(function (towns) {
+            $scope.towns = towns;
+        }, function (error) {
+            notifyService.showError("Load towns failed", error);
+        });
+
+        $scope.register = function (userData) {
+            authService.register(userData,
+                function success(data) {
+                    notifyService.showInfo("Register successful");
+                    $location.path('/');
+                },
+                function error(err) {
+                    notifyService.showError("User registration failed", err);
+                }
+            );
+        };
+    }
+);
---
+++ AngularJS Lab/app/js/services/adsServices.js
@@ -0,0 +1,54 @@
+﻿'use strict';
+app.constant('baseServiceUrl', 'http://softuni-ads.azurewebsites.net');
+
+app.factory('adsService', function ($resource, baseServiceUrl) {
+
+    var adsResource = $resource(
+        baseServiceUrl + '/api/Ads?CategoryId=:categoryId&TownId=:townId&StartPage=:startPage&PageSize=:pageSize',
+        null,
+        {
+            'getAll': {
+                method: 'GET'
+            },
+
+        }
+    );
+
+    return {
+        getAds: function (params, success, error) {
+            return adsResource.getAll(params, success, error);
+        }
+    }
+}
+);
+
+
+app.factory('townsService',
+    function ($resource, baseServiceUrl) {
+        var townsResource = $resource(
+            baseServiceUrl + '/api/towns'
+        );
+
+        return {
+            getTowns: function (success, error) {
+                return townsResource.query(success, error);
+            }
+        }
+    }
+);
+
+
+app.factory('categoriesService',
+    function ($resource, baseServiceUrl) {
+        var categoriesResource = $resource(
+            baseServiceUrl + '/api/categories'
+        );
+
+        return {
+            getCategories: function (success, error) {
+                return categoriesResource.query(success, error);
+            }
+        }
+    }
+);
+
